5c66da1dfd8a7db2dc7bef53ae1ecc2295056f77
Szabó <ladislav.szabo01@t-systems.com>
2024-12-07 10:35:14 +0100

    Part2 start

--- 2024/d7/src/main/java/org/example/Main.java
+++ 2024/d7/src/main/java/org/example/Main.java
@@ -3,22 +3,55 @@ package org.example;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.BinaryOperator;
-import java.util.stream.Collectors;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Pair> test = loadFile("input")
+        List<Pair> test = loadFile("test")
                 .stream().map(
                         Main::parseToPair
                 )
 //                .peek(System.out::println)
                 .toList();
-        test.forEach(e -> combination(e, new ArrayList<>(), 0, new ArrayList<>()));
+//        part1(test);
+        part2(test);
+
+    }
+
+    static Set<Long> set = new HashSet<>();
+    private static void part2(List<Pair> test) {
+        System.out.println(test);
+        ArrayList<Pair> pairs = new ArrayList<>();
+        for (Pair pair : test) {
+            set.clear();
+            if (combination(pair, new ArrayList<>(), 0, new ArrayList<>())){
+                pairs.add(pair);
+            }else {
+                if (pair.arguments().size() == 2 && concatToLong(pair.arguments()).equals(pair.result())){
+                    System.out.println("adding pair " + pair);
+                    pairs.add(pair);
+                }
+                assignCombinations(pair, new ArrayList<>(), 0, new ArrayList<>());
+            }
+            System.out.println(set);
+        }
+
+
+        Long result = pairs.stream()
+                .map(Pair::result)
+                .reduce(Long::sum)
+                .orElseThrow();
+        System.out.println(result);
+    }
+
+    private static Long concatToLong(List<Long> arguments) {
+        StringBuilder stringBuilder = new StringBuilder();
+        arguments.forEach(stringBuilder::append);
+        return Long.parseLong(stringBuilder.toString());
+    }
+
+    private static void part1(List<Pair> test) {
         Long result = test.stream()
                 .filter(e -> combination(e, new ArrayList<>(), 0, new ArrayList<>()))
                 .peek(System.out::println)
@@ -26,7 +59,6 @@ public class Main {
                 .reduce(Long::sum)
                 .orElseThrow();
         System.out.println(result);
-
     }
 
     private static Pair parseToPair(String s) {
@@ -56,6 +88,57 @@ public class Main {
             // process errors
         }
         return lines;
+    }
+
+    static void assignCombinations(
+            Pair pair,
+            List<Long> longs,
+            int i,
+            List<Operator> operators
+    ) {
+        Long sum = 0l;
+        if (longs.size() == pair.arguments().size()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < longs.size(); j++) {
+                Long l = longs.get(j);
+                if (Objects.isNull(operators)) {
+                    sum = 0l;
+                    break;
+                }
+                if (j == 0) {
+                    sum = l;
+                    stringBuilder.append(l + " ");
+                    continue;
+                }
+                Operator operator = operators.get(j);
+                if (operator.equals(Operator.PLUS)) {
+                    sum += l;
+                    stringBuilder.append(" + " + l);
+                }
+                if (operator.equals(Operator.MULTIPLICATION)) {
+                    sum *= l;
+                    stringBuilder.append(" * " + l);
+                }
+            }
+            stringBuilder.append(" = " + sum);
+//            System.out.println(stringBuilder);
+        }
+
+        if (longs.size() == pair.arguments().size()) {
+            set.add(sum);
+            return;
+        }
+        for (int j = i; j < pair.arguments().size(); j++) {
+            Long get = pair.arguments().get(j);
+            longs.add(get);
+            operators.add(Operator.PLUS);
+            assignCombinations(pair, longs, j + 1, operators);
+            operators.remove(operators.size() -1);
+            operators.add(Operator.MULTIPLICATION);
+            assignCombinations(pair, longs, j + 1, operators);
+            operators.remove(operators.size() -1);
+            longs.remove(longs.size() -1 );
+        }
     }
 
     static boolean combination(
