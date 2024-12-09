a8037d35f32948baa5cae89c3dee4a7aa50dcae5
Szabó <ladislav.szabo01@t-systems.com>
2024-12-09 11:13:48 +0100

    save

--- 2024/d9/src/main/java/org/example/Main.java
+++ 2024/d9/src/main/java/org/example/Main.java
@@ -18,10 +18,99 @@ public class Main {
 
             }
         }
-        part1(parsed);
+        part2(parsed);
 
 
     }
+
+
+    private static void part2(List<String> parsed) {
+        List<String> strings = new ArrayList<>();
+        int fileCounter = 0;
+        Integer idCounter = 0;
+
+        for (String s : parsed) {
+            int n = Integer.parseInt(s);
+            if (fileCounter % 2 == 0){
+                for (int i = 0; i<n;i++){
+                    strings.add(idCounter.toString());
+                }
+                idCounter++;
+            }else {
+                for (int i = 0; i<n;i++){
+                    strings.add(".");
+                }
+            }
+            fileCounter++;
+        }
+        System.out.println(strings);
+        Deque<Node> nodes = compress(strings);
+
+        System.out.println(nodes);
+
+        Deque<Node> finalString = process(nodes);
+
+        System.out.println(finalString);
+
+
+    }
+
+    private static Deque<Node> process(Deque<Node> nodes) {
+        int emptySpaceCounter = 0;
+        Deque<Node> finalString = new ArrayDeque<>();
+        while(!nodes.isEmpty()){
+            Node first = nodes.pollFirst();
+            if (first.isFile()){
+                finalString.add(first);
+                continue;
+            }
+            Node last = nodes.peekLast();
+            System.out.println(last);
+            if (Objects.isNull(last)) {
+                finalString.add(first);
+                continue;
+            }
+
+            if (first.n() < last.n()){
+                finalString.add(first);
+                continue;
+            }
+            last = nodes.pollLast();
+            emptySpaceCounter+=last.n();
+            int deltaDots = first.n() - last.n();
+            finalString.add(last);
+            if (deltaDots > 0) {
+                finalString.add(new Node(false, ".", deltaDots));
+            }
+        }
+        if (emptySpaceCounter > 0) {
+            finalString.add(new Node(false, ".", emptySpaceCounter));
+        }
+        return finalString;
+    }
+
+    private static Deque<Node> compress(List<String> strings) {
+        int lastCounter = 1;
+        Deque<Node> nodes = new ArrayDeque<>();
+        for (int i = 1; i < strings.size(); i++) {
+            String previous = strings.get(i - 1);
+            String current = strings.get(i);
+//            System.out.println("previous " + previous + " current " + current);
+            if (!previous.equals(current)){
+                boolean isFile = ! current.equals(".") ? false : true;
+                nodes.add(new Node(isFile,previous, lastCounter));
+                lastCounter = 1;
+            }else {
+                lastCounter++;
+            }
+            if (i == strings.size() - 1){
+                boolean isFile = ! current.equals(".") ? false : true;
+                nodes.add(new Node(isFile,previous, lastCounter));
+            }
+        }
+        return nodes;
+    }
+
 
     private static void part1(List<String> parsed) {
         Deque<String> strings = new ArrayDeque<>();
---
+++ 2024/d9/src/main/java/org/example/Node.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public record Node(boolean isFile, String s, int n) {
+}
